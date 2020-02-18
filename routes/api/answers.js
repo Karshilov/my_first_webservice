c0ad2dd0e21e52f4679b75bcd40a5690a3c2d3c1
Karshilov <karshilov@foxmail.com>
2020-02-18 22:50:50 +0800

    format

--- routes/api/answers.js
+++ routes/api/answers.js
@@ -1,39 +1,43 @@
 const mysql = require('../../middleware/mysql.js');
+
 exports.route = {
     async post(data = []) {
         const ret = data;
-        console.log("success enter");
-        console.log(data);
-        console.log(ret);
-        for (var i = 0; i < ret.keys.length; i++) {
-            if (typeof ret[`${i}`] === 'string') {
-                const typeOfAnswer = ret[`${i}`][0] + 'answer';
-                console.log(`select ${typeOfAnswer} from questions`);
-                let form = await this.db.execute(`select ${typeOfAnswer} from Q${i + 1}`);
-                console.log(form);
-                if (ret[`${i}`][0] != 'O') {
-                    const nowCounter = form.rows[0][0] + 1;
-                    let change = await this.db.execute(`update Q${i + 1} set ${typeOfAnswer}=${nowCounter}`);
+        for (var i = 0; i < 20; i++) {
+            if (typeof ret[i.toString()] === 'string') {
+                const typeOfAnswer = ret[i.toString()][0] + 'answer';
+                //console.log(ret[i.toString()]);
+                let form = await this.db.execute(`select ${typeOfAnswer} from questions where id=${i + 1}`);
+                if (ret[i.toString()][0] != 'O') {
+                    //console.log("gkdgkd");
+                    const nowCounter = form[0][0][`${typeOfAnswer}`] + 1;
+                    let change = await this.db.execute(`update questions set ${typeOfAnswer}=${nowCounter} where id=${i + 1}`);
                 }
                 else {
-                    const nowCounter = form.rows[0][0] + '\n' + ret[`${i}`].substring(2);
-                    let change = await this.db.execute(`update Q${i + 1} set ${typeOfAnswer}=${nowCounter}`);
+                    //console.log("what happened?");
+                    const nowCounter = form[0][0][`${typeOfAnswer}`] + '   ' + ret[i.toString()].substring(2);
+                    let change = await this.db.execute(`update questions set ${typeOfAnswer}='${nowCounter}' where id=${i + 1}`);
                 }
             }
             else {
-                for (var str in ret[`${i}`]) {
+                for (var j = 0; j < ret[i.toString()].length; j++) {
+                    const str = ret[i.toString()][j];
+                    //console.log(`???? ${str}`);
                     const typeOfAnswer = str[0] + 'answer';
-                    let form = await this.db.execute(`select ${typeOfAnswer} from Q${i + 1}`);
+                    let form = await this.db.execute(`select ${typeOfAnswer} from questions where id=${i + 1}`);
                     if (str[0] != 'O') {
-                        const nowCounter = form.rows[0][0] + 1;
-                        let change = await this.db.execute(`update Q${i + 1} set ${typeOfAnswer}=${nowCounter}`);
+                        //console.log("gkdgkd");
+                        const nowCounter = form[0][0][`${typeOfAnswer}`] + 1;
+                        let change = await this.db.execute(`update questions set ${typeOfAnswer}=${nowCounter} where id=${i + 1}`);
                     }
                     else {
-                        const nowCounter = form.rows[0][0] + '\n' + ret[`${i}`].substring(2);
-                        let change = await this.db.execute(`update Q${i + 1} set ${typeOfAnswer}=${nowCounter}`);
+                        //console.log("what happened?");
+                        const nowCounter = form[0][0][`${typeOfAnswer}`] + '\n' + ret[i.toString].substring(2);
+                        let change = await this.db.execute(`update questions set ${typeOfAnswer}='${nowCounter}' where id=${i + 1}`);
                     }
                 }
             }
         }
+        return 'success';
     }
 }
