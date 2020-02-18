469c50c12481d27c940e4ece949e9d9dc17b1440
Karshilov <56143706+Karshilov@users.noreply.github.com>
2020-02-18 13:51:05 +0800

    Update default.js

--- database/default.js
+++ database/default.js
@@ -1,10 +1,10 @@
 const config = {
     sqlBase: {
-        host: '118.190.175.206',
-        user: 'root',
-        password: "LKP'18Contest2D",
-        port: 3306,
-        database: 'answerData',
+        host: '???',
+        user: '???',
+        password: "???",
+        port: ???,
+        database: '???',
     }
 }
 
